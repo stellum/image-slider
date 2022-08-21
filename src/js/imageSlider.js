0c72e1a892d5bb1e7ae31cc99503e6465b2797dc
stellum <stellaloh16@gmail.com>
2022-08-21 19:14:21 +0900

    feat : moveToRight and moveToLeft event with slideNumber boundary

--- src/js/imageSlider.js
+++ src/js/imageSlider.js
@@ -42,10 +42,28 @@ export default class imageSlider {
 
   addEvent() {
     this.nextBtnEl.addEventListener('click', this.moveToRight.bind(this));
+    this.previousBtnEl.addEventListener('click', this.moveToLeft.bind(this));
   }
 
   moveToRight() {
     this.#currentPosition += 1;
+    // 경계값 0 ~ 6에서 6에서 7로 넘어갈 때 처리해주기
+    if (this.#currentPosition === this.#slideNumber) {
+      // currentPosition 넘버가 slideNumber 갯수랑 같아지면 처음 슬라이드로 보여주기
+      this.#currentPosition = 0;
+    }
+    this.sliderListEl.style.left = `-${
+      this.#slideWidth * this.#currentPosition
+    }px`;
+  }
+
+  moveToLeft() {
+    this.#currentPosition -= 1;
+    // 0에서 -1로 넘어갈 때 처리해주기
+    if (this.#currentPosition === -1) {
+      // currentPosition 넘버가 -1 일때,마지막슬라이드(= this.#slideNumber-1) 보여주기
+      this.#currentPosition = this.#slideNumber - 1;
+    }
     this.sliderListEl.style.left = `-${
       this.#slideWidth * this.#currentPosition
     }px`;
